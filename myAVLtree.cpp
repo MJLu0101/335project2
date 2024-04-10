a0b9bee1cfeef9a2261660d65fee15c9ebf1f208
MJLuke <lmj3326679252@gmail.com>
2024-04-10 06:43:02 -0400

    save

--- myAVLtree.cpp
+++ myAVLtree.cpp
@@ -4,22 +4,33 @@
 void treeMedian (const std::vector<int> * instructions){
     AvlTree smallTree;
     AvlTree largeTree;
+    int smallcount=0;
+    int largecount=0;
     int median;
     for(auto i=(*instructions).begin();i!=(*instructions).end();++i){
         if(*i==-1){
-            if(!smallTree.isEmpty()){
-                median=smallTree.findMax();
-                cout<<median<<" ";
-                smallTree.remove(median);
+            if(!smallTree.isEmpty()&&!largeTree.isEmpty()){
+                if(smallcount>=largecount){
+                    median=smallTree.findMax();
+                    smallTree.remove(median);
+                    cout<<median<<" ";
+                }
+                else if(smallcount<largecount){
+                    median=largeTree.findMin();
+                    largeTree.remove(median);
+                    cout<<median<<" ";
+                }
             }
 
         }
         else{
             if(smallTree.isEmpty()||*i<=smallTree.findMax()){
                 smallTree.insert(*i);
+                smallcount++;
             }
             else{
                 largeTree.insert(*i);
+                largecount++;
             }
         }
     }
